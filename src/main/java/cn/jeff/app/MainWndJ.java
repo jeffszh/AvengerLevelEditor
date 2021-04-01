6fffe9885412746a48c782983aad906f26d4b9c5
Jeff <zhsu@amwares.com>
2021-04-01 18:13:34 +0800

    建好自己喜欢的JK框架，打包运行正常。

---
+++ src/main/java/cn/jeff/app/MainWndJ.java
@@ -0,0 +1,15 @@
+package cn.jeff.app;
+
+public class MainWndJ {
+
+	public MainWnd k;
+
+	public void chooseFile() {
+		k.chooseFile();
+	}
+
+	public void refresh() {
+		k.refresh();
+	}
+
+}
--- src/main/kotlin/cn/jeff/app/MainWnd.kt
+++ src/main/kotlin/cn/jeff/app/MainWnd.kt
@@ -1,15 +1,31 @@
 package cn.jeff.app
 
+import javafx.fxml.FXMLLoader
+import javafx.scene.layout.BorderPane
 import tornadofx.*
 
 class MainWnd : View("复仇魔神角色等级编辑器") {
 
-	override val root = hbox {
-		button("刷新") {
-			action {
-				println("很好！")
-			}
-		}
+	override val root: BorderPane
+	private val j: MainWndJ
+
+	init {
+		primaryStage.isResizable = false
+
+		val loader = FXMLLoader()
+		root = loader.load(javaClass.getResourceAsStream(
+				"/cn/jeff/app/MainWnd.fxml"
+		))
+		j = loader.getController()
+		j.k = this
+	}
+
+	fun chooseFile() {
+		information("标题", "选择文件吧。")
+	}
+
+	fun refresh() {
+		information("刷新")
 	}
 
 }
