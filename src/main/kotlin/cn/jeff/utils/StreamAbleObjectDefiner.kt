ae4928101a8d00f55c3b775eba2e8046921fe950
Jeff <zhsu@amwares.com>
2021-04-14 16:31:10 +0800

    添加 StreamAbleObjectDefiner 类。

---
+++ src/main/kotlin/cn/jeff/utils/StreamAbleObjectDefiner.kt
@@ -0,0 +1,12 @@
+package cn.jeff.utils
+
+/**
+ * # Stream-able object definer
+ *
+ * 用于对象与字节流的双向转换。
+ * ------
+ * 实际工作中经常需要进行诸如字节流数据收发之类，
+ * 因此很有必要做一种通用的方法来处理字节流和Java/Kotlin对象的相互转换。
+ */
+class StreamAbleObjectDefiner {
+}
