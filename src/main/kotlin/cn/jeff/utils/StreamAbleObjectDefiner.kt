eea255bdaa94207740b86d06be1c6c2b39d23446
Jeff <zhsu@amwares.com>
2021-04-16 16:23:44 +0800

    ByteOutputStream不能处理只读字段。

--- src/main/kotlin/cn/jeff/utils/StreamAbleObjectDefiner.kt
+++ src/main/kotlin/cn/jeff/utils/StreamAbleObjectDefiner.kt
@@ -36,88 +36,92 @@ class StreamAbleObjectDefiner(
 	fun saveToByteStream(bs: ByteOutputStream) {
 //		var offset = 0
 		fieldDefs.forEach { fieldDef ->
-			if (!fieldDef.readOnly) { // 跳过只读的字段。
-				// 处理指定偏移量
-				if (fieldDef.hardOffset != null) {
-					bs.write(ByteArray(fieldDef.hardOffset - bs.size()))
-				}
+			// 实际上ByteOutputStream不能跳过若干字节，
+			// 所以saveToByteStream不能处理只读字段。
+			if (fieldDef.readOnly) {
+				throw StreamingException("saveToByteStream()不能处理只读字段。")
+			}
 
-				val field = fieldDef.field.javaField!!
+			// 处理指定偏移量
+			if (fieldDef.hardOffset != null) {
+				bs.write(ByteArray(fieldDef.hardOffset - bs.size()))
+			}
+
+			val field = fieldDef.field.javaField!!
 //				println(field.type)
 
-				when (field.type) {
-					// 整数
-					in setOf(Int::class.java, Long::class.java,
-							java.lang.Integer::class.java, java.lang.Long::class.java) -> {
-						println("${field.name} 是 ${field.type}")
-						// 若没有指定fieldSize，从类型计算出来。
-						val fieldSize = fieldDef.fieldSize
-								?: when (field.type) {
-									in setOf(Int::class.java, java.lang.Integer::class.java) -> 4
-									in setOf(Long::class.java, java.lang.Long::class.java) -> 8
-									else -> throw StreamingException("不可能运行到这里。")
-								}
-						var fieldValue = (fieldDef.field.getter.call(managedObject) as Number).toLong()
-						val ba = ByteArray(fieldSize)
-						val factor = (if (fieldDef.isBcd) 10 else 16).toLong()
-						for (i in ba.indices) {
-							val l = fieldValue % factor
-							fieldValue /= factor
-							val h = fieldValue % factor
-							fieldValue /= factor
-							ba[i] = ((h shl 4) + l).toByte()
-						}
-						if (fieldDef.isBigEndian) {
-							ba.reverse()
-						}
-						bs.write(ba)
+			when (field.type) {
+				// 整数
+				in setOf(Int::class.java, Long::class.java,
+						java.lang.Integer::class.java, java.lang.Long::class.java) -> {
+					println("${field.name} 是 ${field.type}")
+					// 若没有指定fieldSize，从类型计算出来。
+					val fieldSize = fieldDef.fieldSize
+							?: when (field.type) {
+								in setOf(Int::class.java, java.lang.Integer::class.java) -> 4
+								in setOf(Long::class.java, java.lang.Long::class.java) -> 8
+								else -> throw StreamingException("不可能运行到这里。")
+							}
+					val ba = ByteArray(fieldSize)
+					var fieldValue = (fieldDef.field.getter.call(managedObject) as Number).toLong()
+					val factor = (if (fieldDef.isBcd) 10 else 16).toLong()
+					for (i in ba.indices) {
+						val l = fieldValue % factor
+						fieldValue /= factor
+						val h = fieldValue % factor
+						fieldValue /= factor
+						ba[i] = ((h shl 4) + l).toByte()
 					}
-
-					// 定点小数
-					in setOf(Float::class.java, Double::class.java,
-							java.lang.Float::class.java, java.lang.Double::class.java) -> {
-						if (fieldDef.fieldSize == null) {
-							throw StreamingException("${field.name} - 非整数数值必须指定长度！")
-						}
-						if (fieldDef.fractionLen == null) {
-							throw StreamingException("${field.name} - 必须指定小数位数！")
-						}
-						val doubleValue = (fieldDef.field.getter.call(managedObject) as Number).toDouble()
-						var p = 1
-						repeat(fieldDef.fractionLen) {
-							p *= 10
-						}
-						var fieldValue = (doubleValue * p).roundToLong()
-						val ba = ByteArray(fieldDef.fieldSize)
-						val factor = (if (fieldDef.isBcd) 10 else 16).toLong()
-						for (i in ba.indices) {
-							val l = fieldValue % factor
-							fieldValue /= factor
-							val h = fieldValue % factor
-							fieldValue /= factor
-							ba[i] = ((h shl 4) + l).toByte()
-						}
-						if (fieldDef.isBigEndian) {
-							ba.reverse()
-						}
-						bs.write(ba)
+					if (fieldDef.isBigEndian) {
+						ba.reverse()
 					}
+					bs.write(ba)
+				}
 
-					// 字符串
-					String::class.java -> {
-						println("${field.name} 是字符串")
-						val fieldSize = fieldDef.fieldSize ?: throw StreamingException(
-								"${field.name} - 字符串必须指定长度！")
+				// 定点小数
+				in setOf(Float::class.java, Double::class.java,
+						java.lang.Float::class.java, java.lang.Double::class.java) -> {
+					if (fieldDef.fieldSize == null) {
+						throw StreamingException("${field.name} - 非整数数值必须指定长度！")
+					}
+					if (fieldDef.fractionLen == null) {
+						throw StreamingException("${field.name} - 必须指定小数位数！")
+					}
+					val doubleValue = (fieldDef.field.getter.call(managedObject) as Number).toDouble()
+					var p = 1
+					repeat(fieldDef.fractionLen) {
+						p *= 10
+					}
+					var fieldValue = (doubleValue * p).roundToLong()
+					val ba = ByteArray(fieldDef.fieldSize)
+					val factor = (if (fieldDef.isBcd) 10 else 16).toLong()
+					for (i in ba.indices) {
+						val l = fieldValue % factor
+						fieldValue /= factor
+						val h = fieldValue % factor
+						fieldValue /= factor
+						ba[i] = ((h shl 4) + l).toByte()
+					}
+					if (fieldDef.isBigEndian) {
+						ba.reverse()
+					}
+					bs.write(ba)
+				}
+
+				// 字符串
+				String::class.java -> {
+					println("${field.name} 是字符串")
+					val fieldSize = fieldDef.fieldSize ?: throw StreamingException(
+							"${field.name} - 字符串必须指定长度！")
 //					val fieldValue = field.get(managedObject) as String
-						val fieldValue = fieldDef.field.getter.call(managedObject) as String
-						val charset = fieldDef.charset ?: Charsets.UTF_8
-						val ba = fieldValue.toByteArray(charset)
-						if (ba.size < fieldSize) {
-							bs.write(ba)
-							bs.write(ByteArray(fieldSize - ba.size))
-						} else {
-							bs.write(ba, 0, fieldSize)
-						}
+					val fieldValue = fieldDef.field.getter.call(managedObject) as String
+					val charset = fieldDef.charset ?: Charsets.UTF_8
+					val ba = fieldValue.toByteArray(charset)
+					if (ba.size < fieldSize) {
+						bs.write(ba)
+						bs.write(ByteArray(fieldSize - ba.size))
+					} else {
+						bs.write(ba, 0, fieldSize)
 					}
 				}
 			}
